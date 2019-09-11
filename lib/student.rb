beb2dae75ef2716665a84bbf46150746c00f5454
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-09-11 23:38:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,24 @@
-class Student 
+class Student < User
+attr_accessor :knowledge
 
+end 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    knowledge << string
+  end
+  
+  def first_name 
+    
+  end
+  
+  def last_name 
+    
+  end
+  
+  def knowledge 
+    student.knowledge.select do {|knowledge| knowledge == self}
+  end
 end
